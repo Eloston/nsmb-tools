4d7bb1f91880ada42d191a18116b8ed6ca45b282
Eloston <eloston@gmail.com>
2011-12-31 03:12:48 +0000

    ClassID Tool: Added Class ID writing support

--- classidtool/trunk/ovproc.py
+++ classidtool/trunk/ovproc.py
@@ -2,6 +2,7 @@
 # Overlay Processor
 
 import binascii
+import mmap
 
 def ovoffset(Region):
     '''
@@ -25,19 +26,47 @@ def Spritenumchk(Sprite):
     else:
         return 'UNK'
 
-def byteread(OVPath, Sprite, Region):
+def ClassIDcheck(ClassID):
+    '''
+    Check to make sure the ClassID value is valid
+    '''
+    if 0 <= ClassID <= 65536:
+        return ClassID
+    else:
+        return 'UNK'
+
+def ClassIDread(OVPath, Sprite, Region):
     '''    
     Read the ClassID of a specific Sprite and return a decimal representation of it
     '''    
-    Overlay = open(OVPath, mode='rb')
     if Spritenumchk(Sprite) != 'UNK':
+        Overlay = open(OVPath, mode='rb')
         Overlay.seek(ovoffset(Region) + Sprite*2 - 2)
+        Byte2 = binascii.hexlify(Overlay.read(1))
+        Byte1 = binascii.hexlify(Overlay.read(1))
+        Overlay.close()  
+        Value = b''.join([Byte1, Byte2])
+        Value = int(Value, 16)
+        return Value
     else:
-        Overlay.close()
         return 'INVSPRITE'
-    Byte2 = binascii.hexlify(Overlay.read(1))
-    Byte1 = binascii.hexlify(Overlay.read(1))
-    Overlay.close()  
-    Value = b''.join([Byte1, Byte2])
-    Value = int(Value, 16)
-    return Value
+
+def ClassIDwrite(OVPath, Sprite, Region, ClassID):
+    '''
+    Write the decimal representation of a ClassID for a sprite to the overlay
+    '''
+    if (Spritenumchk(Sprite) != 'UNK') & (ClassIDcheck(ClassID) != 'UNK'):
+        ClassID = hex(ClassID)[2:]
+        ZeroAdd = (4 - len(ClassID))*'0'
+        ClassID = ''.join([ZeroAdd, ClassID])
+        Overlay = open(OVPath, mode='r+b')
+        Overlay = mmap.mmap(Overlay.fileno(), 0)
+        Overlay.seek(ovoffset(Region) + Sprite*2 - 2)
+        Overlay.write_byte(int(ClassID[2:], 16))
+        Overlay.seek(ovoffset(Region) + Sprite*2 - 1)
+        Overlay.write_byte(int(ClassID[:2], 16))
+        Overlay.flush()
+        Overlay.close()
+        return "Done"
+    else:
+        return "INVCLASSIDSPRITE"
