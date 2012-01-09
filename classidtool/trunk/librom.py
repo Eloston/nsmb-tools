752e72764cd55def3ede9fa20b7d60d28cbfe9d4
Eloston <eloston@gmail.com>
2012-01-08 19:53:56 -0800

    ClassID Tool: -Added direct ROM dump editing -Updated interface -Updated README and info.txt -Added a patch generating script NSMB Tools: -Added README

---
+++ classidtool/trunk/librom.py
@@ -0,0 +1,100 @@
+# ClassID Tool
+# Parse NSMB ROM
+
+import binascii
+import libmisc
+import ovproc
+import mmap
+
+def detect_region(ROMPath):
+    '''
+    Return game region using the ROM header
+    '''
+    try:        
+        ROM = open(ROMPath, mode='rb')
+    except IOError:
+        return 'Invalid ROM Path'
+    ROM.seek(12)
+    Char1 = hex(int(ROM.read(1), 16))[2:]
+    Char2 = hex(int(ROM.read(1), 16))[2:]
+    Char3 = hex(int(ROM.read(1), 16))[2:]
+    Char4 = hex(int(ROM.read(1), 16))[2:]
+    ROM.close()
+    Gamecode = ''.join([Char1, Char2, Char3, Char4])
+    if Gamecode.upper() == 'A2DE':
+        return 'USA'
+    elif Gamecode.upper() == 'A2DP':
+        return 'EUR'
+    elif Gamecode.upper() == 'A2DJ':
+        return 'JAP'
+    elif Gamecode.upper() == 'A2DK':
+        return 'KOR'
+    else:
+        return 'UNK'
+
+def get_overlay_offset(ROMPath, OVERLAYOffset):
+    '''
+    Get decimal representation of the offset of the Overlay0 from the ROM
+    '''
+    try:        
+        ROM = open(ROMPath, mode='rb')
+    except IOError:
+        return 'Invalid ROM Path'
+    ROM.seek(72)
+    offpt4 = binascii.hexlify(ROM.read(1))
+    offpt3 = binascii.hexlify(ROM.read(1))
+    offpt2 = binascii.hexlify(ROM.read(1))
+    offpt1 = binascii.hexlify(ROM.read(1))
+    offset = b''.join([offpt1, offpt2, offpt3, offpt4])
+    offset = int(offset, 16)
+    ROM.seek(offset)
+    offpt4 = binascii.hexlify(ROM.read(1))
+    offpt3 = binascii.hexlify(ROM.read(1))
+    offpt2 = binascii.hexlify(ROM.read(1))
+    offpt1 = binascii.hexlify(ROM.read(1))
+    offset = b''.join([offpt1, offpt2, offpt3, offpt4])
+    offset = int(offset, 16)
+    ROM.close()
+    return offset+OVERLAYOffset
+
+def ClassIDread(ROMPath, Sprite, OVERLAYOffset):
+    '''
+    Read the Class ID from the Overlay 0 from the ROM
+    '''
+    if libmisc.Spritenumchk(Sprite) != 'UNK':
+        try:        
+            ROM = open(ROMPath, mode='rb')
+        except IOError:
+            return 'Invalid ROM Path'
+        ROM.seek(OVERLAYOffset + Sprite*2 - 2)
+        Byte2 = binascii.hexlify(ROM.read(1))
+        Byte1 = binascii.hexlify(ROM.read(1))
+        ROM.close()  
+        Value = b''.join([Byte1, Byte2])
+        Value = int(Value, 16)
+        return Value
+    else:
+        return 'INVSPRITE'
+
+def ClassIDwrite(ROMPath, Sprite, ClassID, OVERLAYOffset):
+    '''
+    Write the decimal representation of a ClassID for a sprite to the ROM
+    '''
+    if (libmisc.Spritenumchk(Sprite) != 'UNK') & (libmisc.ClassIDcheck(ClassID) != 'UNK'):
+        ClassID = hex(ClassID)[2:]
+        ZeroAdd = (4 - len(ClassID))*'0'
+        ClassID = ''.join([ZeroAdd, ClassID])
+        try:
+            ROM = open(ROMPath, mode='r+b')
+        except IOError:
+            return 'Invalid ROM Path'
+        ROM = mmap.mmap(ROM.fileno(), 20772366)
+        ROM.seek(OVERLAYOffset + Sprite*2 - 2)
+        ROM.write_byte(int(ClassID[2:], 16))
+        ROM.seek(OVERLAYOffset + Sprite*2 - 1)
+        ROM.write_byte(int(ClassID[:2], 16))
+        ROM.flush()
+        ROM.close()
+        return "Done"
+    else:
+        return "INVCLASSIDSPRITE"
--- classidtool/trunk/run.py
+++ classidtool/trunk/run.py
@@ -1,144 +1,39 @@
 # ClassID Tool
-# Run ClassID Tool
+# Friendly interface chooser
 
-import ovproc
-import database
-import libmisc
-import libpatch
+import interfacerom
+import interfaceoverlay
 
-def interface():
-    # Import and store Name Database
-    namedb = database.readdb('NameDatabase')
-    # Import and store Patch Original
-    patchoriginal = database.read_patch('PatchOriginal')
-    # Begin Interface
+def interfacechooser():
     print("ClassID Tool")
     print("By nsmbhacking")
     print("---------------")
-    overlay_path = input("Input the absolute or relative path to the extracted Overlay0: ")
-    print("---------------")
-    rom_region = ovproc.region_detect(overlay_path)
-    if rom_region == 'UNK':
-        print("The Overlay region could not be automatically detected. Specify it manually")    
-        print("Put USA for United States version")
-        print("Put EUR for the European version")
-        print("Put JAP for the Japanese version")
-        print("And put KOR for the Korean version")
-        rom_region = input("Put your version here: ")
-        print("---------------")
-    else:
-        print("Is the Overlay region", rom_region, "correct?")
-        romregionconfirm = input("Press [Enter] to accept, or type N to specify your own: ")
-        if romregionconfirm.upper() == "N":
-            print("Input the ROM Region of the overlay")
-            print("Put USA for United States version")
-            print("Put EUR for the European version")
-            print("Put JAP for the Japanese version")
-            print("And put KOR for the Korean version")
-            rom_region = input("Put your version here: ")
-            print("---------------")
     while True:
-        print("Choose your option:")
-        print("A) Read Class ID")
-        print("B) Write Class ID")
-        print("C) Lookup Class ID")
-        print("D) Change Overlay0 Region")
-        print("E) Change Overlay0 Path")
-        print("F) Restart Class ID Tool")
-        print("G) Quit Class ID Tool")
-        print("H) Import Patch")
-        print("I) Export Patch")
-        menu_option = input("Choose your option: ")
+        print("Choose your interface")
+        print("A) Open NSMB ROM")
+        print("B) Open extracted Overlay0")
+        print("C) Close ClassID Tool")
+        interfacechooser_choice = input("Choose your option: ")
         print("---------------")
-        if menu_option.upper() == "A":
-            while True:            
-                sprite_input = input("Specify the Sprite you want to look up the Class ID of: ")
-                print("---------------")
-                try:
-                    sprite_input = int(sprite_input)
-                    break
-                except ValueError:
-                    print("Not a valid sprite value. Try again")
-                    input("Press [Enter] to continue")
-                    print("---------------")
-            classid_read_result = ovproc.ClassIDread(overlay_path, sprite_input, rom_region)
-            if classid_read_result == 'INVSPRITE':
-                print("Invalid Sprite Number: Out of range")
-                print("---------------")
-            else:
-                print("The Class ID for Sprite", sprite_input, "is", classid_read_result, 'which is named', database.lookup(namedb, classid_read_result))
-                input("Press [Enter] to continue")
-                print("---------------")
-        elif menu_option.upper() == "B":
-            while True:            
-                sprite_input = input("Specify the Sprite you want to change the Class ID of: ")
-                print("---------------")
-                try:
-                    sprite_input = int(sprite_input)
-                    break
-                except ValueError:
-                    print("Not a valid sprite value. Try again")
-                    input("Press [Enter] to continue")
-                    print("---------------")
-            while True:            
-                classid_input = input("Specify the Class ID you want to change the sprite to: ")
-                print("---------------")
-                try:
-                    classid_input = int(classid_input)
-                    break
-                except ValueError:
-                    print("Not a valid Class ID. Try again")
-                    input("Press [Enter] to continue")
-                    print("---------------")
-            classid_write_result = ovproc.ClassIDwrite(overlay_path, sprite_input, rom_region, classid_input)
-            if classid_write_result == "INVCLASSIDSPRITE":
-                print("Your Sprite value and/or Class ID were not valid: Out of Range")
-                input("Press [Enter] to continue")
-                print("---------------")
-            else:
-                print("You wrote", classid_input, "which is", database.lookup(namedb, classid_input), "to Sprite", sprite_input)
-        elif menu_option.upper() == "C":
-            while True:            
-                classid_input = input("Specify the Class ID you want to look the name of up: ")
-                print("---------------")
-                try:
-                    classid_input = int(classid_input)
-                    break
-                except ValueError:
-                    print("Not a valid Class ID. Try again")
-                    input("Press [Enter] to continue")
-                    print("---------------")
-            lookup_result = database.lookup(namedb, classid_input)
-            if lookup_result == "INVCLASSID":
-                print("The Class ID you specified is not valid.")
-                input("Press [Enter] to continue")
-            else:
-                print("The name of the Class ID is:", lookup_result)
-                input("Press [Enter] to continue")
-                print("---------------")
-        elif menu_option.upper() == "D":
-            print("Do option F")
-            input("Press [Enter] to continue")
+        if interfacechooser_choice.upper() == "A":
+            print("*-#-=CAUTION=-#-*")
+            print("The Overlay0 in the ROM MUST be decompressed before you access the Overlay0 in the ROM.")
+            print("NSMBe is capable of decompressing the Overlay0.")
+            print("Without decompressing the Overlay0, this tool may cause severe damage to the ROM, resulting in corruption.")
+            print("Do you want to continue?")
+            nsmbrom_warning_input = input("Type 'Yes' to continue, or hit [Enter] to cancel: ")
             print("---------------")
-        elif menu_option.upper() == "E":
-            print("Do option F")
-            input("Press [Enter] to continue")
-            print("---------------")
-        elif menu_option.upper() == "F":
-            print("Not implemented yet. Do option G and run Class ID Tool again.")
-            input("Press [Enter] to continue")
-            print("---------------")
-        elif menu_option.upper() == "G":
+            if nsmb_rom_warning_input == "Yes":            
+                interfacerom.interface()
+            else:
+                print("Returning to main menu")
+                print("---------------")
+        elif interfacechooser_choice.upper() == "B":
+            interfaceoverlay.interface()
+        elif interfacechooser_choice.upper() == "C":
             quit()
-        elif menu_option.upper() == "H":
-            patch_input = input("Specify the absolute or relative path to the patch: ")
-            patch_input = database.read_patch(patch_input)
-            libpatch.import_patch(patch_input, overlay_path, rom_region)
-        elif menu_option.upper() == "I":
-            patch_input = input("Specify where you want the patch to be written to: ")
-            libpatch.create_patch(patch_input, patchoriginal, overlay_path, rom_region)
         else:
             print("Invalid option")
             input("Press [Enter] to continue")
             print("---------------")
-interface()
+interfacechooser()
